9ac24f455aa58ed1bae5f5950500bb33d6a2dfde
skye rogers <skyero@amazon.com>
2025-06-25 12:08:50 -0700

    fix sed command for shard replacement

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -8,19 +8,19 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
   sed -i "" "s/applicationName = .*/applicationName = $APP_NAME/" samples/basic_sample/consumer/sample.properties
   sed -i "" "s/#*idleTimeBetweenReadsInMillis.*/idleTimeBetweenReadsInMillis = 250/" samples/basic_sample/consumer/sample.properties
   sed -i "" "s/stream : 'kclnodejssample'/stream : '$STREAM_NAME'/" samples/basic_sample/producer/config.js
-  sed -i "" "s/shards : 2'/shards : 1'/" samples/basic_sample/producer/config.js
+  sed -i "" "s/shards : 2/shards : 1/" samples/basic_sample/producer/config.js
 elif [[ "$RUNNER_OS" == "Linux" ]]; then
   sed -i "s/streamName = .*/streamName = $STREAM_NAME/" samples/basic_sample/consumer/sample.properties
   sed -i "s/applicationName = .*/applicationName = $APP_NAME/" samples/basic_sample/consumer/sample.properties
   sed -i "s/#*idleTimeBetweenReadsInMillis.*/idleTimeBetweenReadsInMillis = 250/" samples/basic_sample/consumer/sample.properties
   sed -i "s/stream : 'kclnodejssample'/stream : '$STREAM_NAME'/" samples/basic_sample/producer/config.js
-  sed -i "s/shards : 2'/shards : 1'/" samples/basic_sample/producer/config.js
+  sed -i "s/shards : 2/shards : 1/" samples/basic_sample/producer/config.js
 elif [[ "$RUNNER_OS" == "Windows" ]]; then
   sed -i "s/streamName = .*/streamName = $STREAM_NAME/" samples/basic_sample/consumer/sample.properties
   sed -i "s/applicationName = .*/applicationName = $APP_NAME/" samples/basic_sample/consumer/sample.properties
   sed -i "s/#*idleTimeBetweenReadsInMillis.*/idleTimeBetweenReadsInMillis = 250/" samples/basic_sample/consumer/sample.properties
   sed -i "s/stream : 'kclnodejssample'/stream : '$STREAM_NAME'/" samples/basic_sample/producer/config.js
-  sed -i "s/shards : 2'/shards : 1'/" samples/basic_sample/producer/config.js
+  sed -i "s/shards : 2/shards : 1/" samples/basic_sample/producer/config.js
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
